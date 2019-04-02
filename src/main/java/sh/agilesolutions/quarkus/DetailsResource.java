756f210e248bf5097f85b65b256db9b82dc5b699
Robert Rong <robert.rong@agile-solutions.ch>
2019-04-02 11:50:38 +0200

    refactoring

--- src/main/java/sh/agilesolutions/quarkus/DetailsResource.java
+++ src/main/java/sh/agilesolutions/quarkus/DetailsResource.java
@@ -10,7 +10,7 @@ public class DetailsResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String getDetails() {
+        return "excellent details";
     }
 }
